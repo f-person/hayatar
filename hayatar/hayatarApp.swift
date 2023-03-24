e24c1fa893736afc01065a30e6d328208fa2c301
Arshak Aghakaryan <agakaryan.arshak@gmail.com>
2023-03-25 03:29:13 +0400

    Initial Commit

---
+++ hayatar/hayatarApp.swift
@@ -0,0 +1,17 @@
+//
+//  hayatarApp.swift
+//  hayatar
+//
+//  Created by arshak ‎ on 25.03.23.
+//
+
+import SwiftUI
+
+@main
+struct hayatarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
